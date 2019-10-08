9fda311db3691aff00ce32920f910a9889752173
Yuli Setyo Budi <yuli.setyo@gmail.com>
2019-10-08 20:18:15 +0700

    Add files via upload
    first commit

---
+++ storage/framework/views/0dcf54907221aa797b591a094e91abfc9f9fca96.php
@@ -0,0 +1,62 @@
+<?php $__env->startSection('content'); ?>
+<div class="container" style="margin-top:30px">
+  <!--<div class="table-responsive-xl table-responsive-lg table-responsive-md table-responsive-sm">-->
+ 
+  <div class="row">
+	  <div class="table-responsive-xl">
+		<table class="table">
+		  <thead>
+			<th>No</th>
+			<th>Tanggal</th>
+			<th>Isi Kolom</th>
+		  </thead>
+		</table>
+	  </div>
+  
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <h3>Some Links</h3>
+      <p>Lorem ipsum dolor sit ame.</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+</div>
+
+<!--<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>This is Footer</p>
+</div>-->
+
+<!--</body>
+</html>-->
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
